1fb3a603e45215691be88f9e3ff643e81e67e3cd
going pateno <ghtjd0127@gmail.com>
2021-03-23 16:27:37 +0900

    DB모듈 초기버전과 간단한 가이드라인

--- server/database.js
+++ server/database.js
@@ -1,21 +1,20 @@
 var mysql = require('mysql');
 var connection;
 const db_config = require("./DB.json");
-const Collection = require("collections")
+var Map = require("collections/map");
 
-//NOTE 이렇게해도 되나싶음
+//NOTE 이렇게해도 되나싶음 더 좋은방법 있으면 알려주세요
 global.DB = {
-    // users : new Collection(), 
-    // university : new Collection(), 
+    users : new Map(), 
+    // university : new Map(), 
 };
-
+//데이터베이스가 연결될때 데이터들 패치받기
 async function init() {
-    
     console.log(`DataBase : "${connection.config.host}:${connection.config.port}"`);
     console.log("init실행")
-    // module.exports.fatchUsers();
+    module.exports.fatchUsers();
 }
-
+// 커넥션이 만료되면 다시 연결해주는 코드
 module.exports.handleDisconnect = function() {
     connection = mysql.createConnection(db_config); // Recreate the connection, since
     // the old one cannot be reused.
@@ -54,7 +53,7 @@ class User{ // 사용자 객체 //클레스 구현예제
     }
     async init(data){
         //TODO sql데이터를 파싱해서 객체에 넣어주세요 
-        //this.id = data.User_id //이런식으로
+        this.id = data.User_id //이런식으로
     }
 }
 class University{ // 대학객체
@@ -69,5 +68,18 @@ module.exports.fatchUsers = function() {
             // global.DB.users.set(row.User_ID, user); // 글로벌데이터로 올리기 //콜렉션 모듈을 교체해서 코드 바꿔야됨
         }
         if(err)Logger.json(err);
+    });
+}
+//addUser 예제코드
+//다른 프로젝트에서 그냥 가져온거라서 내용이 좀 이상할꺼임
+module.exports.addUser = function(user,callback){
+    // let date = new Date();
+    //쿼리는 이렇게 작성해주면 됨
+    let qqq = `INSERT INTO \`pateno0127\`.\`User\` (\`User_ID\`, \`isAdmin\`, \`UserName\`, \`Locale\`, \`Discriminator\`, \`eMail\`, \`AccessToken\`, \`RegistDate\`) VALUES ('${user.id}', '0', '${user.username}', '${user.locale}', '${user.discriminator}', '${user.email}', '${user.accessToken}', CURRENT_TIMESTAMP)`;
+    // Logger(qqq); // 로그기능 아직 없으니까 무시
+    connection.query(qqq, function(err, rows, fields) { // DB에 요청보내기
+        if(!err)Logger.json(err); // 로그기능 아직 없으니까 무시2
+        callback(err, rows); // 콜백함수 형태로 구현되있음 async await 형태로 구현하면 더 좋을것같음
+        module.exports.fatchUsers(); // 데이터베이스에 변동이 생겼으니 동기화해주기
     });
 }
