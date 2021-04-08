cda1a9cd457216a11c60f7e2431eed68971d837a
going pateno <ghtjd0127@gmail.com>
2021-04-08 16:55:55 +0900

    레거시 로그인 코드 삭제

--- server/router/main.js
+++ server/router/main.js
@@ -5,13 +5,13 @@ module.exports = function(app, fs)
     app.get('/',function(req,res){
         res.send('Hello World!!!!')
     });
-    app.get('/login/:username',function(req, res) {
-        var ip = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
-        console.log(`[${ip}] ${req.params.username}님이 웹사이트 로그인에 성공하셨습니다.`);
-        req.session.username = req.params.username;
-        console.log(req.user);
-        res.redirect('/session');
-    });
+    // app.get('/login/:username',function(req, res) {
+    //     var ip = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
+    //     console.log(`[${ip}] ${req.params.username}님이 웹사이트 로그인에 성공하셨습니다.`);
+    //     req.session.username = req.params.username;
+    //     console.log(req.user);
+    //     res.redirect('/session');
+    // });
     app.get('/session',function(req,res){
         res.send(req.session)
         console.log(req.user)
