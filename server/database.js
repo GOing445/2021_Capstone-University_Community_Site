76dd58578286c4a8de2a9bbb387f31f3e7f1c07f
going pateno <ghtjd0127@gmail.com>
2021-04-27 16:14:39 +0900

    database모듈 db로부터 user,schedule 받아와서 동기화코드 fatchUser추가 대학객체 삭제

--- server/database.js
+++ server/database.js
@@ -3,16 +3,19 @@ var connection;
 const db_config = require("./config.json").DB;
 var Map = require("collections/map");
 
+
 //NOTE 이렇게해도 되나싶음 더 좋은방법 있으면 알려주세요
 global.DB = {
     users : new Map(), 
-    // university : new Map(), 
+    // university : new Map(),
+    // schedules : new Map(), 
+    // friends : new Map(),
 };
 //데이터베이스가 연결될때 데이터들 패치받기
 async function init() {
     console.log(`DataBase : "${connection.config.host}:${connection.config.port}"`);
     console.log("init실행")
-    // module.exports.fatchUsers();
+    module.exports.fatchUsers();
 }
 // 커넥션이 만료되면 다시 연결해주는 코드
 module.exports.handleDisconnect = function() {
@@ -40,34 +43,64 @@ module.exports.handleDisconnect = function() {
 
 module.exports.handleDisconnect();
 //TODO 클래스들은 나중에 스트럭쳐 파일들로 독립시키기
-class User{ // 사용자 객체 //클레스 구현예제
+class User{ // 사용자 객체
     constructor(data){
         // console.log(data);
         this.id = null;//유저 아이디(기본식별자)
         this.name = null;// 실명
-        this.password = null;// 비밀번호(암호화된)
+        this.invCode = null;// 비밀번호(암호화된)
         this.registDate = null;// 가입일자
-        this.University_ID = null;//TODO 대학객체의 포인터를 연결해주면 좋을것같음
-        this.isSuperAdmin = false; // 사이트 관리자인가
+        this.schedules =[];
+        // if(data) this.init(data);
+    }
+    async init(data){
+        this.id = data.id ;
+        this.name = data.name ;
+        this.invCode = data.invCode ;
+        this.registDate = data.registDate ;
+        this.schedules = await getSchedulesFromUserID(data.id);
+    }
+}
+class schedule{ // 스케줄 객체
+    constructor(data){
+        this.day = null;// 요일 0(일)~6(토)
+        this.className = null;// 강의 이름
+        this.classroom = null;// 강의실
+        this.start = null;// 일정이 시작되는 시간
+        this.end = null;// 일정이 끝나는 시간
         // if(data) this.init(data);
     }
     async init(data){
         //TODO sql데이터를 파싱해서 객체에 넣어주세요 
-        this.id = data.User_id //이런식으로
+        this.id = data.id ;//이런식으로
+        this.name = data.name ;//이런식으로
+        this.invCode = data.invCode ;//이런식으로
+        this.registDate = data.registDate ;//이런식으로
     }
 }
-class University{ // 대학객체
-    //TODO 
-}
 
+getSchedulesFromUserID = function(user_id,callback){
+    return new Promise(function(resolve,reject){
+        //쿼리는 이렇게 작성해주면 됨
+        let qqq = `SELECT * FROM Schedule WHERE owner = "${user_id}"`;
+        // Logger(qqq); // 로그기능 아직 없으니까 무시
+        connection.query(qqq, function(err, rows, fields) { // DB에 요청보내기
+            if(!err)console.log(err); // 로그기능 아직 없으니까 무시2
+            if(callback)callback(err, rows); // 콜백함수 형태로 구현되있음 async await 형태로 구현하면 더 좋을것같음
+            else resolve(rows)
+            // module.exports.fatchUsers(); // 데이터베이스에 변동이 생겼으니 동기화해주기
+        });
+    });
+}
 module.exports.fatchUsers = function() {
     connection.query(`SELECT * FROM User`,function(err, rows, fields) {
         for(row of rows){ // 유저한명씩 객체만들고 글로벌객체로 올려두기
             let user = new User(row) // 새객체
             user.init(row); // 데이터채우기
-            // global.DB.users.set(row.User_ID, user); // 글로벌데이터로 올리기 //콜렉션 모듈을 교체해서 코드 바꿔야됨
+            global.DB.users.set(row.id, user); // 글로벌데이터로 올리기 //콜렉션 모듈을 교체해서 코드 바꿔야됨
         }
-        if(err)Logger.json(err);
+        if(err)console.log(err);
+        console.log(global.DB.users);
     });
 }
 //addUser 예제코드
